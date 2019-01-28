e030bfb8ce10183ff5150a8fefd8e0c660397d34
IVIanuu <IVIanuu@gmail.com>
2019-01-28 15:37:31 +0100

    Migrate to bintray

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -14,7 +14,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.multiprocessprefs"
     const val vcsUrl = "https://github.com/IVIanuu/multi-process-prefs"
-    const val version = "git"
+    const val version = "${Build.versionName}-dev-1"
 }
 
 object Versions {
