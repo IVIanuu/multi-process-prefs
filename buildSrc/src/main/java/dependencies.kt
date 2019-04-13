b1f46c01793dcee78ab834c33ad4f9af6bc7a8d1
IVIanuu <IVIanuu@gmail.com>
2019-04-13 17:46:26 +0200

    Updated deps

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -19,7 +19,7 @@ object Publishing {
 
 object Versions {
     const val androidGradlePlugin = "3.3.0"
-    const val androidxAppCompat = "1.0.2"
+    const val androidxAppCompat = "1.1.0-alpha04"
     const val bintray = "1.8.4"
     const val kotlin = "1.3.30"
     const val mavenGradle = "2.1"
